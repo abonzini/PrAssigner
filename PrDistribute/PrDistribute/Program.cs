42cacea6cf71e32809fe9ecb17a697dfd9e4ab7a
Augusto Bonzini <augusto.bonzini@hitachirail.com>
2026-09-14 11:31:44 +0100

    Highjack this repo into the revcord PR assign code. Basic file structures

---
+++ PrDistribute/PrDistribute/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace PrDistribute
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
